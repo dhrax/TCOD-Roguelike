a7cb6fb1da4105cd4ca1314e5112bff1bfac3a0d
dhrax <davidfdez006@gmail.com>
2022-07-23 10:27:16 +0200

    Random map generator completed (both rooms and tunnels between them)

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from actions import EscapeAction, MovementAction
 from input_handlers import EventHandler
 from entity import Entity
 from engine import Engine
-from game_map import GameMap
+from procgen import generate_dungeon
 
 
 def main() -> None:
@@ -17,10 +17,15 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    game_map = GameMap(width=80, height=50)
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", [255, 255, 255])
     npc = Entity(int(screen_width / 2 - 10), int(screen_height / 2 - 10), "N", [255, 255, 0])
+
+    game_map = generate_dungeon(max_rooms=max_rooms, room_max_size=room_max_size,
+    room_min_size=room_min_size, map_width=80, map_height=50, player=player)
 
     entities = {player, npc}
 
